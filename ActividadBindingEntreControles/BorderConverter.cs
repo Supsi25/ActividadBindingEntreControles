8a0353574678cf629eb3aa93e3d5d0de2f4a9717
susana <susana.casas@iesdoctorbalmis.com>
2020-11-22 14:30:41 +0100

    Agregar archivos de proyecto.

---
+++ ActividadBindingEntreControles/BorderConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ActividadBindingEntreControles
+{
+    class BorderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return 2;
+            else
+                return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
